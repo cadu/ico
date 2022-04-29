1068c1574ddf6a01d2e2a4d3aa25bfca9157a343
cadu <cadu@2km.com.br>
2022-04-29 12:54:41 +0100

    Initial commit

---
+++ hardhat-tutorial/constants/index.js
@@ -0,0 +1,4 @@
+// Address of the NFT Contract that you deployed
+const CRYPTO_DEVS_NFT_CONTRACT_ADDRESS = "0x1815e786f8eb6001bc8f6850851f0ffdb20c459d";
+
+module.exports = { CRYPTO_DEVS_NFT_CONTRACT_ADDRESS };
